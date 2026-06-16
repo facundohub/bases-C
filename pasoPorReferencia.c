85d9a8e42444f5ca1e578c072e0e4446b7e11c61
Facundo Maldonado <159500023+facundohub@users.noreply.github.com>
2026-06-16 18:46:28 -0300

    Add files via upload

---
+++ pasoPorReferencia.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//ponemos * para que la referencia de memoria se acrive
+void cambiarValor(int *parametro){
+	//aca modificamos el valor de la variable con *
+	*parametro = 20;
+}
+	int main(int argc, char *argv[]) {
+		
+		int argumento = 10;
+		printf("antes de llamar a la funcion: %d\n", argumento);
+		//con & se pasa la dir de memoria 
+		cambiarValor(&argumento);
+		printf("despues de llamar a la funcion: %d", argumento);
+		
+		return 0;
+	}
---
+++ pasoPorValor.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void cambiarValor(int parametro){
+	parametro = 20;
+}
+int main(int argc, char *argv[]) {
+	
+	int argumento = 10;
+	printf("antes de llamar a la funcion: %d\n", argumento);
+	cambiarValor(argumento);
+	printf("despues de llamar a la funcion: %d", argumento);
+	
+	return 0;
+}
+
