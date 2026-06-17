f4118c619ee2fbca141261d562c3bbfc79bbd363
Facundo Maldonado <159500023+facundohub@users.noreply.github.com>
2026-06-16 23:35:42 -0300

    Add files via upload

---
+++ cambioValorCadenaPorReferencia.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//cambiar valor cadena por referencia
+char* cambiarValor(char *parametro){
+	parametro = "Adios";
+	return parametro;
+}
+	
+	int main(int argc, char *argv[]) {
+		
+		//char arg[] = "Hola";
+		//tambien es una sintaxis válida
+		char *arg = "hola";
+		printf("antes de llamar a la funcion: %s\n", arg);
+		//los arreglos siempre se pasan por referencia 
+		arg = cambiarValor( arg );
+		printf("despues de llamar a la funcion: %s", arg);
+		//con control R podemos cambiar el nombre de una variable en todo el codigo
+		return 0;
+	}
+
